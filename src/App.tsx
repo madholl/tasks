b05ee861982b0c414d97d47bcb979629cf1cc559
madholl <madholl@udel.edu>
2022-02-16 10:43:50 -0500

    added Hello World to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">UD CISC275</header>
+            <header>Hello World</header>
             <p>Madison Holloway</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
